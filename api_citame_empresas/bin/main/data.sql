20365621be4d226250653047648c666092f7a5c5
Juan Miguel Sánchez Begines <jumisanbeg@gmail.com>
2020-09-03 20:01:37 +0200

    Datos de prueba

---
+++ api_citame_empresas/bin/main/data.sql
@@ -0,0 +1,6 @@
+insert into EMPRESA (id, name, cif) values (1, 'Empresa1', '12345'); 
+insert into EMPRESA (id, name, cif) values (2, 'Empresa2', '12346'); 
+insert into EMPRESA (id, name, cif) values (3, 'Empresa3', '12347'); 
+insert into EMPRESA (id, name, cif) values (4, 'Empresa4', '12348'); 
+insert into EMPRESA (id, name, cif) values (5, 'Empresa5', '12349'); 
+insert into EMPRESA (id, name, cif) values (6, 'Empresa6', '12344'); 
