1328c8ab5758908723232f206e677db9ae5ffad4
jumisanbeg@gmail.com <jumisanbeg@gmail.com>
2021-02-07 03:18:46 +0100

    Se añade la carpeta build

---
+++ api_citame_empresas/build/resources/main/data.sql
@@ -0,0 +1,6 @@
+/*insert into entreprise (id, name, cif) values (1, 'Empresa1', '12345'); 
+*insert into entreprise (id, name, cif) values (2, 'Empresa2', '12346'); 
+*insert into entreprise (id, name, cif) values (3, 'Empresa3', '12347'); 
+*insert into entreprise (id, name, cif) values (5, 'Empresa5', '12349'); 
+*insert into entreprise (id, name, cif) values (6, 'Empresa6', '12344');
+*/
